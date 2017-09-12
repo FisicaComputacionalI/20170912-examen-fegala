1dc6941c76ad58ba110b0d917e91ab6e93008b5c
fegala <31254488+fegala@users.noreply.github.com>
2017-09-12 13:58:19 -0500

    Update ejercicio2.py
    correcciòn de detalles

--- ejercicio2.py
+++ ejercicio2.py
@@ -5,8 +5,8 @@ import numpy as np
 def f(t):
  return ((2 * np.cos(t)) + 2015)
 #definimo el intervalo que va a recorrer y arreglamos cada cuanto se va a agregar un valor
-t1 = np.arange(0.0, 15.0, 0.1)
+t = np.arange(0.0, 15.0, 0.1)
 #Definimos la grafica, color y detalles
-plt.plot(t1, f(t1), 'k+')
+plt.plot(t, f(t), 'k+')
 #Finalmente se guarda la imagen
 plt.savefig('ejercicio2.png')
