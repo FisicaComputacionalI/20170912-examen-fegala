64f2d3675a5065e9a4a0259ba1d2d0dbb89ac088
fegala <31254488+fegala@users.noreply.github.com>
2017-09-12 13:57:55 -0500

    Update ejercicio3.py
    correcciòn de detalles

--- ejercicio3.py
+++ ejercicio3.py
@@ -8,11 +8,11 @@ def f(t):
 x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
 y = [1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017]
 #definimo el intervalo que va a recorrer y arreglamos cada cuanto se va a agregar un valor
-t1 = np.arange(0.0, 15.0, 0.1)
+t = np.arange(0.0, 15.0, 0.1)
 #Dividimos en 2 graficas que ocupan una columna cada una, donde en el primer espacio van las graficas del ejercicio 1 y 2
 plt.subplot(121)
-plt.plot(t1, f(t1), 'k+', x, y)
+plt.plot(t, f(t), 'k+', x, y)
 plt.subplot(122)
-plt.plot(t1, f(t1), t1, f(t1), 'r*')
+plt.plot(t, f(t), t, f(t), 'r*')
 #Finalmente se guarda la imagen
 plt.savefig('ejercicio3.png')
