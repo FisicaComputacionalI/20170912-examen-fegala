1159b2570db5ba2ddacf2402cf29d111f31ec30c
fegala <31254488+fegala@users.noreply.github.com>
2017-09-12 13:48:15 -0500

    Update ExamenFernandoGarza1.py
    Se agregaron apellidos a la grafica

--- ExamenFernandoGarza1.py
+++ ExamenFernandoGarza1.py
@@ -7,7 +7,7 @@ y = [1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 200
 #Definimos la grafica
 pl.plot(x, y, 'r--')
 #Agregamos titulo
-pl.title('Fernando')
+pl.title('Fernando Garza Landa')
 #Colocamos la leyenda 'edad' y 'anio' a 'x' y a 'y' respectivamente
 pl.xlabel('edad')
 pl.ylabel('anio')
